a07900d9a869dfe50fc13168a08df1565793ed1b
Sharad Patel <sharad959595@gmail.com>
2018-08-25 23:09:41 +0530

    Update Application.java

--- src/main/java/com/sharad34/Application.java
+++ src/main/java/com/sharad34/Application.java
@@ -5,19 +5,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @SpringBootApplication is an alternative used for @Configuration, @EnableAutoConfiguration and @ComponentScan
+
+// CommandLineRunner is used to use SpringBoot for a Standalone Application(non-web)
+
 public class Application implements CommandLineRunner{
 
     public static void main(String[] args) throws Exception {
 
-        SpringApplication app = new SpringApplication(Application.class);
-        app.run(args);
+        //to set banner off
+        // springApp.setBannerMode(Banner.Mode.OFF);
+        // springApp.run(args);
 
-    }
+
+        //Use the below code for a non-gui app
+        //SpringApplication app = new SpringApplication(Application.class);
+        //app.run(args);
+
+        //Use the below code for a Swing application
+        // new SpringApplicationBuilder(Application.class)
+        //           .headless(false)
+        //           .run(args);
+
+      }
 
     @Override
     public void run(String... arg0) throws Exception {
 
-	  //write your code here
+      //write code as per the requirement
+
     }
 
 }
